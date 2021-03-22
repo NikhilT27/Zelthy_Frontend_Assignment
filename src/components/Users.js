59dca35af148c4274868813c1aa8fa36d2922c12
NikhilT27 <nikhilthakare14@gmail.com>
2021-03-23 01:19:29 +0530

    add: fetchUserData and map EachUser over it

---
+++ src/components/Users.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import EachUser from "./EachUser";
+import Loading from "./Loading";
+
+export default function Users() {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  const fetchUserData = async () => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+
+    if (!response) {
+      console.log("Loading");
+    }
+
+    if (response) {
+      console.log(response.data);
+      setUserData(response.data);
+    }
+  };
+
+  return (
+    <div className="users-box">
+      {userData.length === 0 ? (
+        <Loading />
+      ) : (
+        userData.map((user) => {
+          return <EachUser key={user.id} data={user} />;
+        })
+      )}
+    </div>
+  );
+}
